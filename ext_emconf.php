6744d40f89aa4982ad570b3a376e4ad5f2a373a4
Benjamin Serfhos <benjamin@serfhos.com>
2017-09-28 15:01:52 +0200

    [TASK] Update dependency contstraint for powermail updates

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,28 +1,29 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Powermail: Static From in Mails',
     'description' => 'Use reply-to as default from address, set static value when configured',
     'category' => 'module',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'state' => 'stable',
     'uploadFolder' => false,
     'clearCacheOnLoad' => true,
+    'createDirs' => '',
     'author' => 'Benjamin Serfhos',
     'author_email' => 'benjamin@serfhos.com',
     'author_company' => 'Rotterdam School of Management, Erasmus University',
-    'constraints' => array(
-        'depends' => array(
-            'typo3' => '6.2.0-7.99.99',
-            'powermail' => '2.6.0-2.99.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-    'autoload' => array(
-        'psr-4' => array(
-            'Serfhos\\PowermailFromOverride\\' => 'Classes'
-        )
-    ),
-);
+    'constraints' => [
+        'depends' => [
+            'typo3' => '6.2.0-8.7.99',
+            'powermail' => '2.6.0-4.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Serfhos\\PowermailFromOverride\\' => 'Classes',
+        ],
+    ],
+];
 
